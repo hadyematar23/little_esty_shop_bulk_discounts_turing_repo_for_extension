28cb2b822c535b26248210469535a0a862700b28
Hady Matar <hadyematar@gmail.com>
2023-03-04 08:33:24 -0700

    US 6 Model working with test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,15 +11,16 @@ class Invoice < ApplicationRecord
   enum status: [:cancelled, 'in progress', :completed]
 
   def total_revenue
-    discount = invoice_items
-      .joins(:bulk_discounts)
-      .where("invoice_items.quantity > bulk_discounts.quantity_threshold")
-      .pluck("bulk_discounts.percentage_discount")
-      .first
+    
+    max = self.invoice_items.select("invoice_items.*, MAX(bulk_discounts.percentage_discount/100) as max_ind_discount").joins(:bulk_discounts).where("invoice_items.quantity > bulk_discounts.quantity_threshold").group(:id)
 
-    self.invoice_items
-      .joins(:bulk_discounts)
-      .where("invoice_items.quantity > bulk_discounts.quantity_threshold")
-      .sum("invoice_items.quantity* (invoice_items.unit_price- (invoice_items.unit_price * #{discount}/100))")
+    y = self.invoice_items.joins(:bulk_discounts).where("invoice_items.quantity > bulk_discounts_invoice_items.quantity_threshold").joins("INNER JOIN (#{max.to_sql}) as max_discounts ON max_discounts.id = invoice_items.id").distinct.sum("invoice_items.quantity* (invoice_items.unit_price- (invoice_items.unit_price * max_discounts.max_ind_discount ))")
+
+    items_with_discounts = self.invoice_items.select("invoice_items.*, MAX(bulk_discounts.percentage_discount) as max_discount").left_joins(:bulk_discounts).group(:id).where("invoice_items.quantity >= bulk_discounts.quantity_threshold")
+
+    z = self.invoice_items.where.not(id: items_with_discounts.ids).sum("quantity*unit_price")
+
+    total = y + z
+
   end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -25,18 +25,39 @@ require 'pry'; binding.pry
     end
 
     it "total_revenue including bulk discounts" do 
-      @merchant1 = Merchant.create!(name: 'Hair Care')
-      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
-      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
-      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
-      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
-      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+      merchant1 = Merchant.create!(name: 'Hair Care')
+      item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant1.id, status: 1)
+      item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: merchant1.id)
+      customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      invoice_1 = Invoice.create!(customer_id: customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      ii_1 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 9, unit_price: 10, status: 2)
+      ii_11 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_8.id, quantity: 1, unit_price: 8, status: 1)
+      ii_12 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_8.id, quantity: 17, unit_price: 5, status: 2)
+      ii_13 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_8.id, quantity: 25, unit_price: 5, status: 2)
 
-      @bulk_discount1 = @merchant1.bulk_discounts.create!(quantity_threshold: 5, percentage_discount: 15)
-      @bulk_discount2 = @merchant1.bulk_discounts.create!(quantity_threshold: 10, percentage_discount: 20)
 
-      expect(@invoice_1.total_revenue).to eq(76.5)
+      bulk_discount1 = merchant1.bulk_discounts.create!(quantity_threshold: 5, percentage_discount: 15)
+      bulk_discount2 = merchant1.bulk_discounts.create!(quantity_threshold: 15, percentage_discount: 20)
+      bulk_discount3 = merchant1.bulk_discounts.create!(quantity_threshold: 20, percentage_discount: 25)
+
+      expect(invoice_1.total_revenue).to eq(246.25)
     end
   end
 end
+
+
+    # discount = (invoice_items.joins(:bulk_discounts).where("invoice_items.quantity > bulk_discounts.quantity_threshold").order(percentage_discount: :desc).pluck("bulk_discounts.percentage_discount").first)/100.0
+
+    # max = self.invoice_items.select("invoice_items.*, MAX(bulk_discounts.percentage_discount) as max_discount").joins(:bulk_discounts).where("invoice_items.quantity > bulk_discounts.quantity_threshold").group(:id)
+    
+    # self.invoice_items
+
+    # self.invoice_items
+    # .joins(:bulk_discounts)
+    # .sum("invoice_items.quantity* (invoice_items.unit_price- (invoice_items.unit_price * 1))")
+
+    # total_revenue = self.invoice_items.sum("quantity*unit_price")
+
+    # items_with_discounts = self.invoice_items.select("invoice_items.*, MAX(bulk_discounts.percentage_discount) as max_discount").left_joins(:bulk_discounts).group(:id).where("invoice_items.quantity >= bulk_discounts.quantity_threshold")
+
+        # select("invoice_items.*, MAX(bulk_discounts.percentage_discount) as max_discount").left_
