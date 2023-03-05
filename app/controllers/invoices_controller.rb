a79346ed32e0193817aaf90a56824038037af7b6
Hady Matar <hadyematar@gmail.com>
2023-03-05 10:35:11 -0700

    Remove pry

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,7 +3,6 @@ class InvoicesController < ApplicationController
   before_action :find_merchant, only: [:index]
 
   def index
-    require 'pry'; binding.pry
     @invoices = @merchant.invoices
   end
 
